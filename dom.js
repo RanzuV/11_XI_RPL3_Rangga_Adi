d23b1945234141627ea84251a40a1cd09c54668b
RanzuV <ranzumiwa@gmail.com>
2024-08-21 16:44:14 +0930

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,17 @@
+const data1 = document.getElementById("data1");
+
+data1.addEventListener("click",function() {
+    alert("Yakin ingin melihat data ini")
+})
+
+const data2 = document.getElementById("data2");
+
+data2.addEventListener("click",function() {
+    alert("Yakin ingin melihat data ini")
+})
+
+const data3 = document.getElementById("data3");
+
+data3.addEventListener("click",function() {
+    alert("Yakin ingin melihat data ini")
+})
